6098591ec1aecfcbf448e479f29d28b5469c8ce3
Mohamed <elkhazen1@gmail.com>
2021-03-21 23:07:24 -0500

    finished first part

--- script.js
+++ script.js
@@ -11,3 +11,36 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function() {
+
+   const button = document.getElementById("formSubmit");
+
+button.addEventListener('click', function(event){
+   event.preventDefault();
+})
+
+   button.addEventListener("click", function() {
+      const pilotName = document.getElementById("pilotName")
+      const copilotName = document.getElementById("copilotName")
+      const fuelLevel = document.getElementById("fuelLevel")
+      const cargoMass = document.getElementById("cargoMass")
+      
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
+         alert('all fields are required.');
+      }
+      if (!isNaN(pilotName.value)){
+         alert('Pilot Name has an invalid entery please use letters only')
+      }else if (!isNaN(copilotName.value)){
+         alert('Copilot Name has an invalid entery please use letters only')
+      }else if (isNaN(fuelLevel.value)){
+         alert('Fule level has an invalid entery please use numbers only')
+      }else if (isNaN(cargoMass.value)){
+         alert('Cargo mass has an invalid entery please use numbers only')
+      }
+
+   });
+
+   
+});
+
