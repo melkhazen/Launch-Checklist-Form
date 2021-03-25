df35f5852367966dc0c3640fcd8dee8c92aae6da
Mohamed <elkhazen1@gmail.com>
2021-03-25 17:48:31 -0500

    fixed fule level

--- script.js
+++ script.js
@@ -80,9 +80,9 @@ window.addEventListener("load", function() {
          faultyItems.style.visibility = "visible";
          launchStatus.style.color = "green";
          launchStatus.innerHTML = `Shuttle is ready for launch`;
-         }else if (fuelLevel.value > 10000) {
+         }else if (Number(fuelLevel.value) > 10000) {
             fuelStatus.innerHTML = "Fuel level high enough for launch"
-         }else if (cargoMass < 10000) {
+         }else if (Number(cargoMass.value) < 10000) {
             cargoMass.innerHTML = "Cargo mass low enough for launch"
          }
    
